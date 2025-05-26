9e6f3f3bef53450b3ffee074bd161c865b7b019a
Jakub Połeć <jkb.polec@gmail.com>
2025-05-26 22:38:16 +0200

    Colored background

--- core/src/main/java/com/project/clicker/MainScene.java
+++ core/src/main/java/com/project/clicker/MainScene.java
@@ -2,7 +2,11 @@ package com.project.clicker;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -10,6 +14,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import com.project.clicker.ui.TableFactory;
@@ -31,6 +36,7 @@ public class MainScene implements Screen {
         clickLabel = new Label("Kliknięcia: 0", skin);
 
         TextButton clickButton = new TextButton("Klik!", skin);
+
         clickButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event,  float x, float y){
@@ -54,7 +60,18 @@ public class MainScene implements Screen {
         clickerTable.add(clickLabel).row();
         clickerTable.add(clickButton);
 
-        mainTable.add(menuTable).expand().left().padTop(50);
+
+
+        Pixmap bgPixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        bgPixmap.setColor(Color.RED); // Ustawienie koloru tła
+        bgPixmap.fill();
+        TextureRegionDrawable textureRegionDrawableBg = new TextureRegionDrawable(new TextureRegion(new Texture(bgPixmap)));
+
+
+
+        clickerTable.setBackground(textureRegionDrawableBg);
+
+        mainTable.add(menuTable).expand().left().center();
         mainTable.add(clickerTable).expand().right().center();
 
         stage.addActor(mainTable);
